c73f9efabf0a52db0c63315659f7378500619b91
Sang Lee <panavrin@gmail.com>
2013-07-28 21:29:52 -0400

    Initial Commit

---
+++ apong/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  apong
+//
+//  Created by Sang Won Lee on 7/28/13.
+//  Copyright Sang Won Lee 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ apong/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  apong
+//
+//  Created by Sang Won Lee on 7/28/13.
+//  Copyright Sang Won Lee 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
